ab41b3160eb802ab0a1beac2fdffc5566d1edca4
James Kern <james@sense.com>
2020-12-06 01:53:40 -0500

    shrank code after figuring out how to do set operations on list of sets

--- day_6/code.py
+++ day_6/code.py
@@ -5,13 +5,8 @@ def part2(groups):
     return sum(map(lambda x: x[1], map(count, groups)))
 
 def count(group):
-    unions, inters = None, None
-
-    for person in group.split("\n"):
-        unions = unions.union(set(list(person))) if unions is not None else set(list(person))
-        inters = inters.intersection(set(list(person))) if inters is not None else set(list(person))
-
-    return len(unions), len(inters)
+    sets = [set(list(person)) for person in group.split("\n")]
+    return len(set.union(*sets)), len(set.intersection(*sets))
 
 if __name__ == "__main__":
     groups = open("input.txt").read().split("\n\n")
